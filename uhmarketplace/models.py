c7b0059f7906280bd801da14f2eb82c4fa2adec7
Kiyoshi Hashida <kiyoshihashida@kiyoshis-mbp.lan>
2021-06-29 19:52:27 -1000

    Seeds.yaml now inludes more users. Models.py updated for the users in seeds.yaml. Search Feature added to textbook

--- uhmarketplace/models.py
+++ uhmarketplace/models.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 class Uhmarketplace(models.Model):
     message = models.CharField(max_length=80)
+
+class User(AbstractUser):
+    pass
 
 class Textbook(models.Model):
     book_title = models.CharField(max_length=80)
@@ -21,4 +24,3 @@ class Textbook(models.Model):
 
     def __str__(self):
         return self.book_title
-
